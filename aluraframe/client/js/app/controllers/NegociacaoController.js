b493346a50108d2df8bec708d7d1670d1631f60a
Pedro Xavier <pedroxxavier@hotmail.com>
2023-09-22 14:57:49 -0300

    Modelo MVC

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,31 @@
+class NegociacaoController {
+  #inputData;
+  #inputQuantidade;
+  #inputValor;
+
+  constructor() {
+    let $ = document.querySelector.bind(document);
+
+    this.#inputData = $("#data");
+    this.#inputQuantidade = $("#quantidade");
+    this.#inputValor = $("#valor");
+  }
+
+  adiciona(event) {
+    event.preventDefault();
+
+    let data = new Date(
+      ...this.#inputData.value
+        .split("-")
+        .map((item, indice) => item - (indice % 2))
+    );
+
+    let negociacao = new Negociacao(
+      data,
+      this.#inputQuantidade.value,
+      this.#inputValor.value
+    );
+
+    console.log(negociacao);
+  }
+}
